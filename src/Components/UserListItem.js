3a10be0341899a3251e7bf68756412dcd5ac1d0f
Anastasia Yanvareva <anastasia.yanvareva@gmail.com>
2018-06-14 08:04:32 +0200

    Remove comments

--- src/Components/UserListItem.js
+++ src/Components/UserListItem.js
@@ -5,7 +5,6 @@ import "../styles/List.css";
 
 
 const UserListItem = props => (
-  // <Link to={`/profile/${props.item.id}`} >
   <Link to={{ pathname: `/profile/${props.item.id}`, state: { username: `${props.item.name}`} }} >
     <CollectionItem className="ListItem">{props.item.name} <Icon right small>chevron_right</Icon></CollectionItem>
   </Link>
